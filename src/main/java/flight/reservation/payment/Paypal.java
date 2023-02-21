852f5e56d4d1b5c6697e5ce26646ec6c8e93be18
BrahmaKorr <122957338+BrahmaKorr@users.noreply.github.com>
2023-02-21 18:30:19 +0530

    Add files via upload

--- src/main/java/flight/reservation/payment/Paypal.java
+++ src/main/java/flight/reservation/payment/Paypal.java
@@ -4,16 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Paypal implements PaymentStrategy{
+    private String email;
+    private String password;
+
+    
     public static final Map<String, String> DATA_BASE = new HashMap<>();
 
+    public Paypal(String email,String password){
+        this.email=null;
+        this.password=null;
+    }
     static {
         DATA_BASE.put("amanda1985", "amanda@ya.com");
         DATA_BASE.put("qwerty", "john@amazon.eu");
     }
 
     @Override
-    public void Pay(Double amount){
+    public boolean Pay(Double amount){
         System.out.println("Paying " + amount + " using PayPal.");
+        return true;
     }
     
 
